85e5dfffc7c2c847390fd9d2f16558b9312893b2
Ana Julia Lopes <anajlsilveira@gmail.com>
2025-03-19 13:25:21 -0300

    Exemplo de interface

---
+++ InterfaceExemplo/src/main/java/com/mycompany/interfaceexemplo/InterfaceExemplo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaceexemplo;
+
+/**
+ *
+ * @author anajl
+ */
+public class InterfaceExemplo {
+
+    public static void main(String[] args) {
+        Animal gato = new Gato();
+        gato.som();
+    }
+}
